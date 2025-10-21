935d91401c62aed369a1fbf84e632775eb137cd1
Vladyslav Hulida <lezver@MacBook-Air-Vladyslav.local>
2025-10-21 11:02:41 +0300

    update tariffs

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,8 @@
 import Hero from "@/components/home/Hero";
 import Materials from "@/components/home/Materials";
 import Tariffs from "@/components/home/Tariffs";
-import { notFound } from "next/navigation";
 
 export default function Home() {
-	// notFound();
-
 	return (
 		<>
 			<Hero />
